757d569919028525865739d10217a3dbeab84815
Amine195 <gmadhgs@gmail.com>
2017-08-03 16:25:40 +0100

    Client panel part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,82 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Route Import
+import { RouterModule, Routes } from '@angular/router';
+
+// Form Import
+import { FormsModule } from '@angular/forms';
+
+// Flash Messages
+import { FlashMessagesModule } from 'angular2-flash-messages';
+
+// AngularFire Imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+// Component Imports
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { AddClientComponent } from './components/add-client/add-client.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+// Service Imports
+import { ClientService } from './services/client.service';
+
+// Setting Routes
+const appRoutes: Routes = [
+  { path:'', component:DashboardComponent },
+  { path:'register', component:RegisterComponent },
+  { path:'login', component:LoginComponent },
+  { path:'add-client', component:AddClientComponent },
+  { path:'client/:id', component:ClientDetailsComponent }
+];
+
+// Config Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyAv9DJ5QTk-w7Wyxzb4ubuK3puXCVcPS0I",
+  authDomain: "clientpanel-2ddae.firebaseapp.com",
+  databaseURL: "https://clientpanel-2ddae.firebaseio.com",
+  storageBucket: "clientpanel-2ddae.appspot.com",
+  messagingSenderId: "965778957878"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ClientsComponent,
+    ClientDetailsComponent,
+    AddClientComponent,
+    EditClientComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LoginComponent,
+    RegisterComponent,
+    SettingsComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes), // Routes
+    AngularFireModule.initializeApp(firebaseConfig), // AngularFire
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth, // AngularFireAuth
+    AngularFireDatabase, // AngularFireDatabase
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
