2dd1af8cd7f3167736ed5d43cb8321996f87b22c
Amine195 <gmadhgs@gmail.com>
2017-08-04 02:41:27 +0100

    Authguard for routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,15 +32,16 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 // Service Imports
 import { ClientService } from './services/client.service';
 import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
 
 // Setting Routes
 const appRoutes: Routes = [
-  { path:'', component:DashboardComponent },
+  { path:'', component:DashboardComponent, canActivate:[AuthGuard] },
   { path:'register', component:RegisterComponent },
   { path:'login', component:LoginComponent },
-  { path:'add-client', component:AddClientComponent },
-  { path:'client/:id', component:ClientDetailsComponent },
-  { path:'edit-client/:id', component:EditClientComponent }
+  { path:'add-client', component:AddClientComponent, canActivate:[AuthGuard] },
+  { path:'client/:id', component:ClientDetailsComponent, canActivate:[AuthGuard] },
+  { path:'edit-client/:id', component:EditClientComponent, canActivate:[AuthGuard] }
 ];
 
 // Config Firebase
@@ -78,7 +79,8 @@ export const firebaseConfig = {
     AngularFireAuth, // AngularFireAuth
     AngularFireDatabase, // AngularFireDatabase
     ClientService,
-    AuthService
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
